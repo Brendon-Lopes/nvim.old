0663b21cfe2c48fbc7ded5564282a0c9ddfc11ca
Brendon <brendon.lopes.21@gmail.com>
2023-07-25 04:02:18 -0300

    feat: add colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "moon",
+      transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+}
+
