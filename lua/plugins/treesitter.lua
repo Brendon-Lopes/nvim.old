1427750ebda8bac45afb0d501d7624d72751345e
Brendon <brendon.lopes.21@gmail.com>
2023-07-26 12:35:30 -0300

    feat: add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			auto_install = true,
+			context_commentstring = { enable = true, enable_autocmd = false },
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = {
+				"bash",
+				"dockerfile",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"python",
+				"regex",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+				"c_sharp",
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+}
