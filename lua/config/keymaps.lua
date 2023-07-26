4667c4dfbb01418f9b5e22eac1611c61c01c5f36
Brendon <brendon.lopes.21@gmail.com>
2023-07-26 20:43:57 -0300

    feat: add which-key and refactor keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,14 @@
-local opts = { noremap = true, silent = true }
-
 -- local term_opts = { silent = true }
 
 -- Shorten function name
-local keymap = vim.keymap.set
+-- local keymap = vim.keymap.set
+
+local function keymap(mode, oldmap, newmap, desc)
+	vim.keymap.set(mode, oldmap, newmap, { noremap = true, silent = true, desc = desc })
+end
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -20,65 +22,63 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
 
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+keymap("n", "<leader>e", ":Lex 30<cr>")
 
 -- Better viewing
-keymap("n", "n", "nzzzv", opts)
-keymap("n", "N", "Nzzzv", opts)
-keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
 
 -- Better pasting (does not yank)
-keymap("v", "p", '"_dP', opts)
-keymap("x", "p", '"_dP', opts)
+keymap("v", "p", '"_dP')
+keymap("x", "p", '"_dP')
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>")
+keymap("n", "<C-Down>", ":resize +2<CR>")
+keymap("n", "<C-Left>", ":vertical resize -2<CR>")
+keymap("n", "<C-Right>", ":vertical resize +2<CR>")
 
 -- Navigate buffers
-keymap("n", "<A-l>", ":bnext<CR>", opts)
-keymap("n", "<A-h>", ":bprevious<CR>", opts)
+keymap("n", "<A-l>", ":bnext<CR>")
+keymap("n", "<A-h>", ":bprevious<CR>")
 
 -- Move text up and down
-keymap("n", "<A-j>", ":m .+1<CR>==", opts)
-keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+keymap("n", "<A-j>", ":m .+1<CR>==")
+keymap("n", "<A-k>", ":m .-2<CR>==")
 
 -- Select entire file
-keymap("n", "<C-a>", "gg<S-v>G", opts)
+keymap("n", "<C-a>", "gg<S-v>G")
+
+-- Buffers
+keymap("n", "<leader>bb", "<cmd>e #<cr>", "Switch to last buffer")
 
 -- Insert --
 -- Press jk fast to exit insert mode
--- keymap("i", "jk", "<ESC>", opts)
--- keymap("i", "kj", "<ESC>", opts)
+-- keymap("i", "jk", "<ESC>")
+-- keymap("i", "kj", "<ESC>")
 
 -- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv^", opts)
-keymap("v", ">", ">gv^", opts)
+keymap("v", "<", "<gv^")
+keymap("v", ">", ">gv^")
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- Visual Block --
 -- Move text up and down
-keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
-keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
-keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
-
--- TODO: refactor
-
--- Buffer
-keymap("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to last buffer", noremap = true, silent = true })
+keymap("x", "J", ":m '>+1<CR>gv=gv")
+keymap("x", "K", ":m '<-2<CR>gv=gv")
+keymap("x", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap("x", "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- Terminal --
 -- Better terminal navigation
