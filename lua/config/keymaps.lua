05f12f2dff3f092e993e468b56ac86a52a33e77d
Brendon <brendon.lopes.21@gmail.com>
2023-07-26 12:20:42 -0300

    feat: update telescope configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -56,8 +56,8 @@ keymap("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", "kj", "<ESC>", opts)
+-- keymap("i", "jk", "<ESC>", opts)
+-- keymap("i", "kj", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,4 +17,29 @@ return {
 		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
 		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Buffers" },
 	},
+	opts = function()
+		local actions = require("telescope.actions")
+
+		return {
+			defaults = {
+				prompt_prefix = " ",
+				selection_caret = " ",
+
+				mappings = {
+					i = {
+						["<C-n>"] = actions.cycle_history_next,
+						["<C-p>"] = actions.cycle_history_prev,
+
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+
+						["<esc>"] = actions.close,
+					},
+				},
+			},
+		}
+	end,
+	config = function(_, opts)
+		require("telescope").setup(opts)
+	end,
 }
