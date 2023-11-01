cacc03b305289315d5b577c1ed63aa7d006c8077
Brendon <brendon.lopes.21@gmail.com>
2023-10-31 22:18:35 -0300

    feat: update configs

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,81 +1,95 @@
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+  return function(str)
+    local win_width = vim.fn.winwidth(0)
+    if hide_width and win_width < hide_width then
+      return ""
+    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+      return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+    end
+    return str
+  end
+end
+
 return {
-	"nvim-lualine/lualine.nvim",
-	enabled = true,
-	lazy = false,
-	event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "auto",
-				icons_enabled = false,
-				section_separators = "",
-				component_separators = "",
-				disabled_filetypes = {
-					statusline = {
-						"help",
-						"startify",
-						"dashboard",
-						"neo-tree",
-						"packer",
-						"neogitstatus",
-						"NvimTree",
-						"Trouble",
-						"alpha",
-						"lir",
-						"Outline",
-						"spectre_panel",
-						"toggleterm",
-						"qf",
-					},
-					winbar = {},
-				},
-			},
-			sections = {
-				lualine_a = {},
-				lualine_b = { "branch" },
-				lualine_c = {
-					{
-						"filetype",
-						icon_only = true,
-						separator = "",
-						padding = {
-							left = 1,
-							right = 0,
-						},
-					},
-					{
-						"filename",
-						path = 1,
-						symbols = {
-							modified = "  ",
-							readonly = "",
-							unnamed = "",
-						},
-					},
-					{
-						"diagnostics",
-						sources = { "nvim_lsp" },
-						symbols = { error = " ", warn = " ", info = " " },
-					},
-				},
-				lualine_x = { "encoding" },
-				lualine_y = {},
-				lualine_z = {
-					function()
-						return " " .. os.date("%R")
-					end,
-				},
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			extensions = { "neo-tree", "lazy" },
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  enabled = true,
+  lazy = false,
+  event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        icons_enabled = false,
+        section_separators = "",
+        component_separators = "",
+        disabled_filetypes = {
+          statusline = {
+            "help",
+            "startify",
+            "dashboard",
+            "neo-tree",
+            "packer",
+            "neogitstatus",
+            "NvimTree",
+            "Trouble",
+            "alpha",
+            "lir",
+            "Outline",
+            "spectre_panel",
+            "toggleterm",
+            "qf",
+          },
+          winbar = {},
+        },
+      },
+      sections = {
+        lualine_a = {
+          { "branch", fmt = trunc(120, 12, 60) },
+        },
+        lualine_b = {},
+        lualine_c = {
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = {
+              left = 1,
+              right = 0,
+            },
+          },
+          {
+            "filename",
+            path = 1,
+            symbols = {
+              modified = "  ",
+              readonly = "",
+              unnamed = "",
+            },
+          },
+          {
+            "diagnostics",
+            sources = { "nvim_lsp" },
+            symbols = { error = " ", warn = " ", info = " " },
+          },
+        },
+        lualine_x = { "encoding" },
+        lualine_y = {},
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = { "neo-tree", "lazy" },
+    })
+  end,
 }
